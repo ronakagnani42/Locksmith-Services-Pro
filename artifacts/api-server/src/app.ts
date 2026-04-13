f7a55126eae1f133f0e5fd8658aa717553f0ae0a
Ronak Agnani <53546383+ronakagnani42@users.noreply.github.com>
2026-04-14 02:28:21 +0530

    new commit4

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,32 +1,14 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
-import * as pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-const httpLogger = (pinoHttp as any).default || pinoHttp;
-
-app.use(
-  httpLogger({
-    logger,
-    serializers: {
-      req(req: any) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: any) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info({ method: req.method, url: req.url?.split("?")[0] }, "request");
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
